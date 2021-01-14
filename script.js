daac87dc53cd3a63b57068c5f0495f86555735a6
koichi-inoue <inoue.ko@gmail.com>
2021-01-14 15:24:21 +0900

    Script Updated

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-window.onload = function() {
-  // EventListener for Drop Image
+// Preload
+const  classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+let video;
+
+// modelLoaded > Ready to Accept
+function modelLoaded() {
+  document.getElementById('message').innerHTML = '<p>The MobileNet model loaded!</p>';
+
+  video = document.getElementById('video');
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then((stream) => {
+      video.srcObject = stream;
+      video.play();
+    })
+
   var obj = document.getElementById("button");
-  obj.addEventListener("click", function(ev){ ev.preventDefault(); prediction(ev);}, false);
+  obj.addEventListener("click", function(ev){ ev.preventDefault(); Classify();}, false);
 }
 
-// Grab elements, create settings, etc.
-const video = document.getElementById('video');
+// Classify
+function Classify(){
 
-// Create a webcam capture
-navigator.mediaDevices.getUserMedia({ video: true })
-  .then((stream) => {
-    video.srcObject = stream;
-    video.play();
-  })
+  classifier.predict(video, DisplayResults);
 
-function prediction(){
+}
 
-  document.getElementById('predictions').innerHTML = '<p>The MobileNet model loading・・</p>';
-  
-  ml5.imageClassifier('MobileNet', video)
-      .then(classifier => classifier.classify())
-      .then(results => {
-        var txt="";
-        for(i=0; i<results.length; i++){
-         txt += "Label：" + results[i].label +'<br>';
-         txt += "Confidence：" + results[i].confidence.toFixed(4) +'<br>';
-         txt += '<br>';
-        }
-        document.getElementById('predictions').innerHTML = txt;
-      });
+function DisplayResults(err, results) {
+
+  var txt="";
+  for(i=0; i<results.length; i++){
+   txt += "Label：" + results[i].label +'<br>';
+   txt += "Confidence：" + results[i].confidence.toFixed(4) +'<br>';
+   txt += '<br>';
+  }
+  document.getElementById('predictions').innerHTML = txt;
 
 }
